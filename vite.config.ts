bdccb7ce3f277b246e1a39fdb4cbc2714006a5fb
Krishna Singhal <singhalkrishna790@gmail.com>
2026-06-04 03:24:27 +0530

    switch to SPA mode

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
-    server: { entry: "server" },
-    nitro: true,
+    spa: {},
   },
 });
